755fbcb6c86f82ffa409a883a3f04323f2a813ab
= <janni.hollmann@gmail.com>
2024-10-31 12:43:26 +0100

    renamed acceleration

--- 01_Nbody/lib/Euler_Cromer.cpp
+++ 01_Nbody/lib/Euler_Cromer.cpp
@@ -9,11 +9,11 @@ class Euler_Cromer : public Integrator {
             
             for(Body current_body : *bodies)
             {
-                std::vector<double> accelaration = tools::calc_acceleration(*bodies,current_body);
+                std::vector<double> acceleration = tools::calc_acceleration(*bodies,current_body);
                 Body new_current_body = current_body;
-                new_current_body.setVx(current_body.getVx() +  accelaration[0]* maxTimeStep);
-                new_current_body.setVy(current_body.getVy() +  accelaration[1]* maxTimeStep);
-                new_current_body.setVz(current_body.getVz() +  accelaration[2]* maxTimeStep);
+                new_current_body.setVx(current_body.getVx() +  acceleration[0]* maxTimeStep);
+                new_current_body.setVy(current_body.getVy() +  acceleration[1]* maxTimeStep);
+                new_current_body.setVz(current_body.getVz() +  acceleration[2]* maxTimeStep);
                 new_current_body.setX(current_body.getX() + new_current_body.getVx() * maxTimeStep);
                 new_current_body.setY(current_body.getY() + new_current_body.getVy() * maxTimeStep);
                 new_current_body.setZ(current_body.getY() + new_current_body.getVz() * maxTimeStep);
--- 01_Nbody/lib/Hermite.cpp
+++ 01_Nbody/lib/Hermite.cpp
@@ -10,16 +10,16 @@ class Hermite : public Integrator {
             {
                 Body predicted_body = current_body;
                 Body corrected_body = current_body;
-                Vector3d initial_accelaration = tools::calc_acceleration_3dvec(*bodies,current_body);
+                Vector3d initial_acceleration = tools::calc_acceleration_3dvec(*bodies,current_body);
                 Vector3d initial_jerk = tools::calc_jerk_3dvec(*bodies,current_body);
-                predicted_body.setVelocity(current_body.getVelocity() + initial_accelaration * maxTimeStep + initial_jerk * 0.5 * pow(maxTimeStep,2));
-                predicted_body.setPosition(current_body.getPosition() + current_body.getVelocity() * maxTimeStep + initial_accelaration * 0.5 * pow(maxTimeStep,2) + initial_jerk *(1/6)* pow(maxTimeStep,3));
-                Vector3d predicted_accelaration = tools::calc_acceleration_3dvec(*bodies,predicted_body);
+                predicted_body.setVelocity(current_body.getVelocity() + initial_acceleration * maxTimeStep + initial_jerk * 0.5 * pow(maxTimeStep,2));
+                predicted_body.setPosition(current_body.getPosition() + current_body.getVelocity() * maxTimeStep + initial_acceleration * 0.5 * pow(maxTimeStep,2) + initial_jerk *(1/6)* pow(maxTimeStep,3));
+                Vector3d predicted_acceleration = tools::calc_acceleration_3dvec(*bodies,predicted_body);
                 Vector3d predicted_jerk = tools::calc_jerk_3dvec(*bodies,predicted_body);
-                Vector3d second_derivative_accelaration = ((initial_accelaration - predicted_accelaration)*(-3)/(pow(maxTimeStep,2)) - (((initial_jerk * 2)-predicted_jerk)/(maxTimeStep) )) * 2;
-                Vector3d third_derivative_accelaration = (((initial_accelaration - predicted_accelaration)*(2)/(pow(maxTimeStep,3))) + ((initial_jerk-predicted_jerk)/(pow(maxTimeStep,2))))*6;
-                corrected_body.setVelocity(predicted_body.getVelocity() + second_derivative_accelaration *(1/6) * pow(maxTimeStep,3) + third_derivative_accelaration * (1/24) * pow(maxTimeStep,4));
-                corrected_body.setPosition(predicted_body.getPosition() + second_derivative_accelaration * (1/24) * pow(maxTimeStep,4) + third_derivative_accelaration * (1/120) * pow(maxTimeStep,5));
+                Vector3d second_derivative_acceleration = ((initial_acceleration - predicted_acceleration)*(-3)/(pow(maxTimeStep,2)) - (((initial_jerk * 2)-predicted_jerk)/(maxTimeStep) )) * 2;
+                Vector3d third_derivative_acceleration = (((initial_acceleration - predicted_acceleration)*(2)/(pow(maxTimeStep,3))) + ((initial_jerk-predicted_jerk)/(pow(maxTimeStep,2))))*6;
+                corrected_body.setVelocity(predicted_body.getVelocity() + second_derivative_acceleration *(1/6) * pow(maxTimeStep,3) + third_derivative_acceleration * (1/24) * pow(maxTimeStep,4));
+                corrected_body.setPosition(predicted_body.getPosition() + second_derivative_acceleration * (1/24) * pow(maxTimeStep,4) + third_derivative_acceleration * (1/120) * pow(maxTimeStep,5));
                 newBodies.push_back(corrected_body);
             }
             return newBodies;
