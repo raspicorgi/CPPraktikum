22de24909fbaae3ecd0af245c5dc4590ba943303
= <janni.hollmann@gmail.com>
2024-10-31 14:54:55 +0100

    Implement default integrate method in Integrator class (meant to be overwritten)

--- 01_Nbody/lib/Integrator.cpp
+++ 01_Nbody/lib/Integrator.cpp
@@ -1,3 +1,8 @@
 #include "Integrator.hpp"
 
-    Integrator::~Integrator() = default;
+Integrator::~Integrator() = default;
+
+std::vector<Body> Integrator::integrate(const std::vector<Body>* bodies, const double maxTimeStep) {
+    // Default implementation, meant to be overwritten
+    return std::vector<Body>();
+}
