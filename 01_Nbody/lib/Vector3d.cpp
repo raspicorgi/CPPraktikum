0a7002a46485b3b090928224431022e250218e60
= <janni.hollmann@gmail.com>
2024-10-31 15:14:22 +0100

    added documentation to Vector3d

--- 01_Nbody/lib/Vector3d.cpp
+++ 01_Nbody/lib/Vector3d.cpp
@@ -1,4 +1,3 @@
-// Vector3d.cpp
 #include "Vector3d.hpp"
 
 // Constructors
@@ -18,7 +17,26 @@ void Vector3d::setZ(double z) { this->z = z; }
 // Member functions
 double Vector3d::magnitude() const { return std::sqrt(x * x + y * y + z * z); }
 Vector3d Vector3d::normalize() const { double mag = magnitude(); return Vector3d(x / mag, y / mag, z / mag); }
+
+/**
+ * @brief Computes the scalar (dot) product of this vector with another vector.
+ * 
+ * The scalar product is defined as the sum of the products of the corresponding components of the two vectors.
+ * 
+ * @param other The vector to compute the scalar product with.
+ * @return The scalar product of this vector and the other vector.
+ */
 double Vector3d::scalar_product(Vector3d other) const { return (x * other.x + y * other.y + z * other.z); }
+
+/**
+ * @brief Computes the cross product of this vector with another vector.
+ * 
+ * The cross product (a x b) is computed, where a is the vector on which the function is called.
+ *  
+ * @param other The vector to compute the cross product with.
+ * @return A new Vector3d representing the cross product of this vector and the other vector.
+ */
+Vector3d Vector3d::cross_product(Vector3d other) const { return Vector3d(y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x); }
 
 // Operator overloads
 Vector3d Vector3d::operator+(const Vector3d& other) const { return Vector3d(x + other.x, y + other.y, z + other.z); }
--- 01_Nbody/lib/Vector3d.hpp
+++ 01_Nbody/lib/Vector3d.hpp
@@ -24,6 +24,7 @@ public:
     double magnitude() const;
     Vector3d normalize() const;
     double scalar_product(Vector3d other) const;
+    Vector3d cross_product(Vector3d other) const;
 
     // Operator overloads
     Vector3d operator+(const Vector3d& other) const;
