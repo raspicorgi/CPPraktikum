248ec26ff017556b99aa6293fbc296effa816b0b
= <janni.hollmann@gmail.com>
2024-10-31 14:52:38 +0100

    fixed multiple definition issue by declaring context of integrate() in Euler.cpp

--- 01_Nbody/lib/Euler.cpp
+++ 01_Nbody/lib/Euler.cpp
@@ -1,6 +1,6 @@
 #include "Euler.hpp"
 
-    std::vector<Body> integrate(const std::vector<Body>* bodies, const double maxTimeStep){
+    std::vector<Body> Euler::integrate(const std::vector<Body>* bodies, const double maxTimeStep){
         std::vector<Body> newBodies;
         // TODO: Implement the Euler integration method
         return newBodies;
