2711bb139704aecae53587a242fb3c480747d1b5
ElSascha <sascha-eckstein@outlook.com>
2024-11-02 19:58:33 +0100

    tested values

--- 01_Nbody/main.cpp
+++ 01_Nbody/main.cpp
@@ -67,7 +67,7 @@ void convertToCenterOfMassSystem(std::vector<Body>& bodies) {
 
 int main() {
     //prefs
-    int iterations = 10;
+    int iterations = 628;
     long double maxTimeStep = 0.01;
 
     //Load data
