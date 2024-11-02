226f7e6e3455e7784b633df02652c21e0835f6ca
Jannis <janni.hollmann@gmail.com>
2024-11-02 16:02:30 +0100

    minor cleanup

--- 01_Nbody/lib/Velocity_Verlet.cpp
+++ 01_Nbody/lib/Velocity_Verlet.cpp
@@ -16,14 +16,6 @@ std::vector<Body> Velocity_Verlet::integrate(const std::vector<Body>& bodies, co
     for (const Body& body : bodies) {
         body_map[body.getId()] = body;
     }
-    // for(const Body& current_body : bodies){
-    //     Body new_current_body(current_body.getId(), current_body.getMass());
-    //     Vector3d acceleration = Tools::calc_acceleration(bodies, current_body);
-    //     new_current_body.setPosition(current_body.getPosition() + current_body.getVelocity() * maxTimeStep + acceleration * 0.5 * pow(maxTimeStep,2));
-    //     Vector3d new_acceleration = Tools::calc_acceleration(bodies,new_current_body);
-    //     new_current_body.setVelocity(current_body.getVelocity() + (acceleration + new_acceleration) * 0.5 * maxTimeStep);
-    //     new_bodies.push_back(new_current_body);
-    // } 
 
     // loop to calculate new positions
     for (const Body& body : bodies){
