eeccf0a5a87c7d3c60d32f1d66b85ae16946b670
ElSascha <sascha-eckstein@outlook.com>
2024-11-07 19:43:35 +0100

    tried to fix rk4

--- 01_Nbody/lib/RK4.cpp
+++ 01_Nbody/lib/RK4.cpp
@@ -2,42 +2,58 @@
 
 std::vector<Body> RK4::integrate(const std::vector<Body>& Bodies, const long double maxTimeStep){
     std::vector<Body> new_Bodies;
-    std::unordered_map<int, Body> k1_map; 
-    std::unordered_map<int, Body> k2_map; 
-    std::unordered_map<int, Body> k3_map; 
-    std::unordered_map<int, Body> k4_map; 
+    std::vector<Vector3d> k1_v;
+    std::vector<Vector3d> k1_r;
+    std::vector<Vector3d> k2_v;
+    std::vector<Vector3d> k2_r;
+    std::vector<Vector3d> k3_v;
+    std::vector<Vector3d> k3_r;
+    std::vector<Vector3d> k4_v;
+    std::vector<Vector3d> k4_r; 
+    std::vector<Body> k1_bodies;
+    std::vector<Body> k2_bodies;
+    std::vector<Body> k3_bodies;
     for(const Body& current_body : Bodies){
         Body k1_body = current_body;
         Vector3d k1v= Tools::calc_acceleration(Bodies, current_body) * maxTimeStep;
         Vector3d k1r= current_body.getVelocity() * maxTimeStep;
-        k1_body.setVelocity(k1v);
-        k1_body.setPosition(k1r);
-        k1_map[current_body.getId()] = k1_body;
+        k1_v.push_back(k1v);
+        k1_r.push_back(k1r);
+        k1_body.setVelocity(current_body.getVelocity() + k1v*0.5);
+        k1_body.setPosition(current_body.getPosition() + k1r*0.5);
+        k1_bodies.push_back(k1_body);
     }
-    for(const Body& current_body : Bodies){}
-    for(const Body& current_body : Bodies){}
-    for(const Body& current_body : Bodies){}
-    for(const Body& current_body : Bodies){}
-
     for(const Body& current_body : Bodies){
-        Body new_current_body = current_body;
-        Vector3d k1v = Tools::calc_acceleration(Bodies, current_body) * maxTimeStep;
-        Vector3d k1r = current_body.getVelocity() * maxTimeStep;
-        new_current_body.setVelocity(current_body.getVelocity() + k1v / 2);
-
-        Vector3d k2v = Tools::calc_acceleration(Bodies,new_current_body)* maxTimeStep;
-        Vector3d k2r = (current_body.getVelocity() + k1v * 0.5)*maxTimeStep;
-        new_current_body.setVelocity(current_body.getVelocity() + k2v / 2);
-
-        Vector3d k3v = Tools::calc_acceleration(Bodies,new_current_body)* maxTimeStep;
-        Vector3d k3r = (current_body.getVelocity() + k2v * 0.5)*maxTimeStep;
-        new_current_body.setVelocity(current_body.getVelocity() + k3v);
-        
-        Vector3d k4v = Tools::calc_acceleration(Bodies,new_current_body)* maxTimeStep;
-        Vector3d k4r = (current_body.getVelocity() + k3v)*maxTimeStep;
-        new_current_body.setVelocity(current_body.getVelocity() + (k1v + k2v*2 + k3v*2 + k4v)*maxTimeStep / 6);
-        new_current_body.setPosition(current_body.getPosition() + (k1r + k2r*2 + k3r*2 + k4r)*maxTimeStep / 6);
-        new_Bodies.push_back(new_current_body);
+        Body k2_body = current_body;
+        Vector3d k2v = Tools::calc_acceleration(k1_bodies, k1_bodies[current_body.getId()]) * maxTimeStep;
+        Vector3d k2r = (current_body.getVelocity() + k1_v[current_body.getId()]*0.5) * maxTimeStep;
+        k2_v.push_back(k2v);
+        k2_r.push_back(k2r);
+        k2_body.setVelocity(current_body.getVelocity() + k2v*0.5);
+        k2_body.setPosition(current_body.getPosition() + k2r*0.5);
+        k2_bodies.push_back(k2_body);
+    }
+    for(const Body& current_body : Bodies){
+        Body k3_body = current_body;
+        Vector3d k3v = Tools::calc_acceleration(k2_bodies, k2_bodies[current_body.getId()]) * maxTimeStep;
+        Vector3d k3r = (current_body.getVelocity() + k2_v[current_body.getId()]*0.5) * maxTimeStep;
+        k3_v.push_back(k3v);
+        k3_r.push_back(k3r);
+        k3_body.setVelocity(current_body.getVelocity() + k3v);
+        k3_body.setPosition(current_body.getPosition() + k3r);
+        k3_bodies.push_back(k3_body);
+    }
+    for(const Body& current_body : Bodies){
+        Vector3d k4v = Tools::calc_acceleration(k3_bodies, k3_bodies[current_body.getId()]) * maxTimeStep;
+        Vector3d k4r = (current_body.getVelocity() + k3_v[current_body.getId()]) * maxTimeStep;
+        k4_v.push_back(k4v);
+        k4_r.push_back(k4r);
+    }
+    for(const Body& current_body : Bodies){
+        Body new_body = current_body;
+        new_body.setVelocity(current_body.getVelocity() + k1_v[current_body.getId()]/6 + k2_v[current_body.getId()]/3 + k3_v[current_body.getId()]/3 + k4_v[current_body.getId()]/6);
+        new_body.setPosition(current_body.getPosition() + k1_r[current_body.getId()]/6 + k2_r[current_body.getId()]/3 + k3_r[current_body.getId()]/3 + k4_r[current_body.getId()]/6); 
+        new_Bodies.push_back(new_body);
     }
     return new_Bodies;
 }
