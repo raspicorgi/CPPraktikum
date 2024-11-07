a1090109f11d5c664eae41880c0afde90e19f4d6
Jannis <janni.hollmann@gmail.com>
2024-11-07 13:05:58 +0100

    added comments

--- 01_Nbody/lib/Heun.cpp
+++ 01_Nbody/lib/Heun.cpp
@@ -3,14 +3,14 @@
 
 std::vector<Body> Heun::integrate(const std::vector<Body>& bodies, const long double maxTimeStep){
     std::vector<Body> newBodies;
-    std::unordered_map<int, Body> k1_map; // I'm just using a Body to store the positions and velocities together
-    std::unordered_map<int, Body> k2_map;
+    std::unordered_map<int, Body> k1_map; // I'm just using a Body to store the positions and velocities together.
+    std::unordered_map<int, Body> k2_map; // The body has no physical meaning, I just use it as a data structure.
 
     // loop to calculate k1 values
     for (const Body& body : bodies){
         Body k1_body(body.getId(), body.getMass());
-        Vector3d k1_velocity = Tools::calc_acceleration(bodies, body) * maxTimeStep;
-        Vector3d k1_position = body.getVelocity() * maxTimeStep;
+        Vector3d k1_velocity = Tools::calc_acceleration(bodies, body) * maxTimeStep; // k1_v
+        Vector3d k1_position = body.getVelocity() * maxTimeStep;                     // k1_r
         k1_body.setVelocity(k1_velocity);
         k1_body.setPosition(k1_position);
         k1_map[body.getId()] = k1_body;
@@ -21,8 +21,8 @@ std::vector<Body> Heun::integrate(const std::vector<Body>& bodies, const long do
         Body k2_body(body.getId(), body.getMass());
         Body moved_body = body;
         moved_body.setPosition(body.getPosition() + k1_map[body.getId()].getPosition());
-        Vector3d k2_velocity = Tools::calc_acceleration(bodies, moved_body) * maxTimeStep;
-        Vector3d k2_position = (body.getVelocity() + k1_map[body.getId()].getVelocity()) * maxTimeStep;
+        Vector3d k2_velocity = Tools::calc_acceleration(bodies, moved_body) * maxTimeStep;              // k2_v
+        Vector3d k2_position = (body.getVelocity() + k1_map[body.getId()].getVelocity()) * maxTimeStep; // k2_r
         k2_body.setVelocity(k2_velocity);
         k2_body.setPosition(k2_position);
         k2_map[body.getId()] = k2_body;
