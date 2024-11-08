91a09309812785d1130da04f439950ea70fc6541
ElSascha <sascha-eckstein@outlook.com>
2024-11-08 18:22:24 +0100

    Finished File writing, added python script

--- 01_Nbody/main.cpp
+++ 01_Nbody/main.cpp
@@ -40,17 +40,30 @@ void normalizeMasses(std::vector<Body>& bodies) {
     }
 }
 
-void simulate(std::vector<Body>& bodies, int iterations, long double maxTimeStep, Integrator& integrator, const std::string& filename) {
-     for (const Body& body : bodies) {
-        body.printState();
-    }
-    // main loop
+void simulate(std::vector<Body>& bodies, int iterations, long double maxTimeStep, Integrator& integrator, const std::string& filename,const std::string& integratorName) {
+    std::ofstream outputFile(filename); // create a new output file or overwrite an existing one
     std::vector<Body> newBodies;
+    outputFile << "# column style format: id time x y z vx vy vz mass\n# this file will be overwritten without any precaution \n# used integrator was " + integratorName + "\n";
+  if (outputFile.is_open()) {
+    // main loop
     for (int i = 1; i <= iterations; i++) {
         newBodies =integrator.integrate(bodies, maxTimeStep);
         std::cout << "Iteration " << i << "/" << iterations << std::endl;
         bodies = newBodies;
+        for(Body current_body : newBodies){
+            outputFile <<current_body.getId()<<";"<<(maxTimeStep*i)<<";"<<current_body.getPosition().getX()<<";"<<current_body.getPosition().getY()<<";"<<current_body.getPosition().getZ()<<";"<<current_body.getVelocity().getX()<<";"<<current_body.getVelocity().getY()<<";"<<current_body.getVelocity().getZ()<<"\n";
+        }
     }
+    outputFile.close(); // close the file when done
+    std::cout << "Data was written to output.txt\n";
+  }
+  else {
+    std::cerr << "Error opening file\n";
+  }
+     for (const Body& body : bodies) {
+        body.printState();
+    }
+    
     std::cout << "Simulation complete." << std::endl;
     for (const Body& body : newBodies) {
         body.printState();
@@ -86,16 +99,17 @@ void convertToCenterOfMassSystem(std::vector<Body>& bodies) {
 
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <integrator>" << std::endl;
+    if (argc < 3) {
+        std::cerr << "Please enter an integrator and the maximal time step in this order, expample: ./name integrator maxTimeStep" << std::endl;
         return 1;
     }
 
+    
     std::string argument = argv[1];
     
     //prefs
     int iterations = 628;
-    long double maxTimeStep = 0.01;
+    long double maxTimeStep = std::stoi(argv[2]);
 
     //Load data
     std::cout << "Loading data... ";
@@ -117,23 +131,23 @@ int main(int argc, char *argv[]) {
     Heun Heun;
     RK4 RK4;
     if (argument == "euler") {
-        simulate(bodies, iterations, maxTimeStep, Euler,"simulated_data/2b_euler_" + std::to_string(maxTimeStep) +".txt");
+        simulate(bodies, iterations, maxTimeStep, Euler,"simulated_data/2b_euler_" + std::to_string(maxTimeStep) +".txt",argument);
     } else if (argument == "euler_cromer") {
-        simulate(bodies, iterations, maxTimeStep, Euler_Cromer,"simulated_data/2b_euler_cromer_" + std::to_string(maxTimeStep) +".txt");
+        simulate(bodies, iterations, maxTimeStep, Euler_Cromer,"simulated_data/2b_euler_cromer_" + std::to_string(maxTimeStep) +".txt",argument);
     } else if (argument == "velocity_verlet") {
-        simulate(bodies, iterations, maxTimeStep, Velocity_Verlet,"simulated_data/2b_velocity_verlet_" + std::to_string(maxTimeStep) +".txt");
+        simulate(bodies, iterations, maxTimeStep, Velocity_Verlet,"simulated_data/2b_velocity_verlet_" + std::to_string(maxTimeStep) +".txt",argument);
     } else if (argument == "hermite") {
-        simulate(bodies, iterations, maxTimeStep, Hermite,"simulated_data/2b_hermite_" + std::to_string(maxTimeStep) +".txt");
+        simulate(bodies, iterations, maxTimeStep, Hermite,"simulated_data/2b_hermite_" + std::to_string(maxTimeStep) +".txt",argument);
     } else if (argument == "iterierter_hermite") {
-        simulate(bodies, iterations, maxTimeStep, Iterierter_Hermite,"simulated_data/2b_iterierter_hermite_" + std::to_string(maxTimeStep) +".txt");
+        simulate(bodies, iterations, maxTimeStep, Iterierter_Hermite,"simulated_data/2b_iterierter_hermite_" + std::to_string(maxTimeStep) +".txt",argument);
     } else if (argument == "heun") {
-        simulate(bodies, iterations, maxTimeStep, Heun,"simulated_data/2b_heun_" + std::to_string(maxTimeStep) +".txt");
+        simulate(bodies, iterations, maxTimeStep, Heun,"simulated_data/2b_heun_" + std::to_string(maxTimeStep) +".txt",argument);
     } else if (argument == "rk4") {
-        simulate(bodies, iterations, maxTimeStep, RK4,"simulated_data/2b_rk4_" + std::to_string(maxTimeStep) +".txt");
+        simulate(bodies, iterations, maxTimeStep, RK4,"simulated_data/2b_rk4_" + std::to_string(maxTimeStep) +".txt",argument);
     } else {
         std::cerr << "Unknown integrator: " << argument << std::endl;
         return 1;
     }
-
+    std::cout << argv[2] <<"\n"<<std::endl;
     return 0;
 }
