766af9b9bb6c8f4ce7ad99d29f0e52766a513c8f
ElSascha <sascha-eckstein@outlook.com>
2024-11-21 13:47:20 +0100

    startet Classical density functional theory

---
+++ 03_classical_density_functional_theory/functions.cpp
@@ -0,0 +1,5 @@
+#include "functions.hpp"
+
+double phi_0D(double x){
+    return (x + (1-x)*log(1-x));
+}
---
+++ 03_classical_density_functional_theory/main.cpp
@@ -0,0 +1,6 @@
+#include "functions.hpp"
+#include <iostream>
+ int main(){
+    std::cout << "phi_0d(0.5) = " << phi_0D(0.5) << std::endl;
+    return 0;
+ }
