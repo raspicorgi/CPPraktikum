323abc63f381e6709e4ef5d332c3f6d7558f5ce9
El_Sascha <sascha.eckstein@uni-tuebingen.de>
2025-01-17 10:10:49 +0100

    GK_Monte_Carlo

---
+++ 06_GK_Monte_Carlo/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <tuple>
+
+int main(){
+    int occ[64][64] = {{0}};
+    std::tupel<int[64],int> L_H;
+    std::tupel<int[64],int> int L_V = ; 
+    return 0;
+}
