bed46efde09854b6a02dbc94240b4ace12ac6b42
raspicorgi <janni.hollmann@gmail.com>
2025-03-11 21:29:58 +0100

    parallelized

--- 04_Ising/Aufgabe_3/3a.py
+++ 04_Ising/Aufgabe_3/3a.py
@@ -1,4 +1,5 @@
 import Ising_A3_lib as ising
+from joblib import Parallel, delayed
 import matplotlib.pyplot as plt
 
 
@@ -11,11 +12,15 @@ betas = [0.1, 0.5, 1]
 N_try = 100
 N_therm = 1000
 
-for beta in betas:
+def process_beta(beta):
     # Initialisiere Gitter
     lattice = ising.init_random_lattice(L)
     energies, lattice = ising.thermalize(lattice, h, J, beta, N_try, N_therm)
+    return beta, energies
 
+results = Parallel(n_jobs=-1)(delayed(process_beta)(beta) for beta in betas)
+
+for beta, energies in results:
     # Plotte Energie
     plt.plot(energies, label=f"beta = {beta}")
 plt.xlabel("Thermalisierungsschritt")
