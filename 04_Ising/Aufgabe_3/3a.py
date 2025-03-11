296cfa1c5da66f7e25df4990254428e54fed022b
raspicorgi <janni.hollmann@gmail.com>
2025-03-11 21:27:20 +0100

    minor changes in plotting

--- 04_Ising/Aufgabe_3/3a.py
+++ 04_Ising/Aufgabe_3/3a.py
@@ -7,17 +7,19 @@ N = 100
 L = 20
 h = 0
 J = 1
-beta = 1
+betas = [0.1, 0.5, 1]
 N_try = 100
 N_therm = 1000
 
-# Initialisiere Gitter
-lattice = ising.init_random_lattice(L)
-energies, lattice = ising.thermalize(lattice, h, J, beta, N_try, N_therm)
+for beta in betas:
+    # Initialisiere Gitter
+    lattice = ising.init_random_lattice(L)
+    energies, lattice = ising.thermalize(lattice, h, J, beta, N_try, N_therm)
 
-# Plotte Energie
-plt.plot(energies)
+    # Plotte Energie
+    plt.plot(energies, label=f"beta = {beta}")
 plt.xlabel("Thermalisierungsschritt")
 plt.ylabel("Energie")
 plt.title("Energie des Gitters nach Thermalisierung")
+plt.legend()
 plt.show()
