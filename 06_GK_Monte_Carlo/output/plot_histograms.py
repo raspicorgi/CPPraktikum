a8ad579d358b1202dd2bcfab3cbedd7b3eff654e
Jannis <janni.hollmann@gmail.com>
2025-03-19 18:51:43 +0100

    updated separate histograms

--- 06_GK_Monte_Carlo/output/plot_histograms.py
+++ 06_GK_Monte_Carlo/output/plot_histograms.py
@@ -10,16 +10,26 @@ files_and_titles = [
     ('06_GK_Monte_Carlo/output/observables_2.csv', 'z = 1.1')
 ]
 
+rename_dict = {
+    'total_rods': '# Gesamt',
+    'horizontal_rods': '# Horizontal',
+    'vertical_rods': '# Vertikal',
+    'eta': '$\eta$',
+    'S': 'S'
+}
+
 for file, title in files_and_titles:
     df = pd.read_csv(file, comment='#')
-    # Create subplots
-    fig, axes = plt.subplots(1, 5, figsize=(15, 3))
+
+
+    fig, axes = plt.subplots(1, 5, figsize=(12, 3))
     columns = [col for col in df.columns if col != 'step']
+
+
     for ax, column in zip(axes, columns):
-        # Plot the data
         ax.hist(df[column], bins=60, density=True)
-        ax.set_title(column, fontsize=16)
-    fig.suptitle(f'Histogramme für {title}')#, fontsize=20)
+        ax.set_title(rename_dict[column], fontsize=14)
+    fig.suptitle(f'Histogramme für {title}')
     plt.tight_layout()
     plt.savefig(f'06_GK_Monte_Carlo/output/histograms_{title}.png')
     plt.close()
