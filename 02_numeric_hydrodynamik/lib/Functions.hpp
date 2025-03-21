bdc5fdf3b2afdc59c917a3e5111e8838bc63e9b0
El_Sascha <sascha.eckstein@uni-tuebingen.de>
2025-03-21 12:30:03 +0100

    Aufgabe 2 erledigt

--- 02_numeric_hydrodynamik/lib/Functions.hpp
+++ 02_numeric_hydrodynamik/lib/Functions.hpp
@@ -2,13 +2,24 @@
 #define FUNCTIONS_HPP
 #include <vector>
 #include <iostream>
+#include <cmath>
+#include <algorithm>
 using std::vector;
 
 class Functions{
     public:
-        static vector<double> advection(vector <double> &psi, int N , double dt, double dx, vector <double> u);
-        static double calc_delta_psi(vector <double> &psi, int N, int j);
+        static vector<double> advection(vector <double> psi, double N , double dt, double dx, vector <double> u, double t_max);
+        static vector<double> rho_advection(vector <double> rho, double N , double dt, double dx, vector <double> u);
+        static vector<double> u_advection(vector <double> u, double N , double dt, double dx);
+        static vector<double> epsilon_advection(vector <double> epsilon, double N , double dt, double dx, vector <double> u);
+        static vector<double> calc_F(vector <double> rho, vector <double> u, vector <double> epsilon, double N, double dt, double dx);
+        static vector<vector<double>> solve_shocktube(vector <double> rho, vector <double> u, vector <double> epsilon, vector<double> p, double N, double dt, double dx, double t_max , double gamma);
+        static double calc_delta_rho(vector <double> rho, double N, int j);
+        static double calc_delta_u(vector <double> u, double N, int j);
+        static double calc_delta_epsilon(vector <double> epsilon, double N, int j);
         static void print_vector(vector<double> &vec);
+
+
     private:
 
 };
--- 02_numeric_hydrodynamik/main.cpp
+++ 02_numeric_hydrodynamik/main.cpp
@@ -9,31 +9,64 @@
 #include <cstring>
 using std::string;
 
-//Parameters
-double a = 1.0;
-double N = 40.0;
-double x_min = -1.0;
-double x_max = 1.0;
-double dx = (x_max - x_min)/N;
-double sigma = 0.8;
-double dt = sigma*dx/a;
-double t_max = 4.0;
+
+
+// Funktion um die Precision der doubles für die Ausgabe zu verringern 
+std::string double_to_string(double &variable)
+{
+   std::ostringstream Str;
+   Str.precision(1);
+   Str <<std::fixed<< variable;
+   return(Str.str());
+}
 
 void write_to_file(vector<double> vec, string filename){
     std::ofstream file(filename);
     for(vector<double>::size_type i = 0; i < vec.size(); i++){
+        if(i == vec.size()-1){
+            file << vec[i];
+            break;
+
+        }
+        else{
         file << vec[i] << ";";
+        }
     }
     file.close();
 }
 
 int main(){
+//Parameters
+double a = 1.0;
+double N = 40.0;
+double x_min = -1.0;
+double x_max = 1.0;
+double sigma = 0.9;
+double t_max = 4.0;
+
+// std::cout<<"Bitte geschwindigkeit a eingeben: "<<std::endl;
+// std::cin>>a;
+// std::cout<<"Bitte Anzahl der Zellen N eingeben: "<<std::endl;
+// std::cin>>N;
+// std::cout<<"Bitte x_min eingeben: "<<std::endl;
+// std::cin>>x_min;
+// std::cout<<"Bitte x_max eingeben: "<<std::endl;
+// std::cin>>x_max;
+// std::cout<<"Bitte sigma eingeben: "<<std::endl;
+// std::cin>>sigma;
+// std::cout<<"Bitte t_max eingeben: "<<std::endl;
+// std::cin>>t_max;
+
+
+double dx = (x_max - x_min)/N;
+double dt = sigma*dx/a;
+
 // Geister Zellen
-vector<double> psi(N+4,0.0);
-vector<double> u(N+5,1.0);
+vector<double> psi(N+4,0.0); // Psi läuft von 0 bis N+3 also insgemsamt N+4 Elemente
+vector<double> u(N+4,0.0);
 // Zell ränder
 vector<double> x_a(N+4,0.0);
-for(int i = 0; i < N+1; i++){
+for(int i = 0; i <= N; i++){
    x_a[i+2] = x_min + (i)*dx;
 }
 // Zell mitten 
@@ -42,8 +75,8 @@ for(int i = 0; i < N; i++){
    x_b[i+2] = x_min + 0.5*dx + i*dx;
 }
 // Rechteckimpuls
-for(int j = 2; j < N+2 ; j++){
-    if(abs(x_b[j]) <= 0.33333){
+for(int j = 2; j <= N+2 ; j++){
+    if(abs(x_b[j]) <= 0.333333333333333333){
         psi[j] = 1.0;
     }
     else{
@@ -51,17 +84,22 @@ for(int j = 2; j < N+2 ; j++){
     }
     
 }
-auto start = std::chrono::high_resolution_clock::now();
-
-for(double t = 0.0; t < t_max; t+=dt){
-    psi = Functions::advection(psi, N, dt, dx, u);
+// Geschwindigkeit
+for(int j = 2; j <= N+2; j++){
+    u[j] = a;
 }
+
+auto start = std::chrono::high_resolution_clock::now();
+vector<double> psi_new(N+4,0.0);
+psi_new = Functions::advection(psi, N, dt, dx, u, t_max);
+
 
 auto end = std::chrono::high_resolution_clock::now();
 std::chrono::duration<double, std::milli> float_ms = end - start;
 std::cout << "Laufzeit: " << float_ms.count() << " ms\n";
-write_to_file(psi, "output/psi_t4_N40.txt");
-write_to_file(x_b, "output/x_b.txt");
-write_to_file(x_a, "output/x_a.txt");
+write_to_file(psi_new, "output/psi_t"+ double_to_string(t_max)+"s_N"+double_to_string(N)+"_sigma"+double_to_string(sigma) +".txt");
+write_to_file(x_b, "output/xb_t"+double_to_string(t_max)+"_N"+ double_to_string(N) +"_sigma"+ double_to_string(sigma) +".txt");
+write_to_file(x_b, "output/xa_t"+double_to_string(t_max)+"_N"+ double_to_string(N) +"_sigma"+ double_to_string(sigma) +".txt");
+
 return 0;
 }
